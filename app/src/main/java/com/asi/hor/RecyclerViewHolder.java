fe2900748b03102639ff69b6c84fb058ceec93ac
ascom <programmerasi@gmail.com>
2016-06-23 13:39:30 +0200

    ADD Anim to RV

--- app/src/main/java/com/asi/hor/RecyclerViewHolder.java
+++ app/src/main/java/com/asi/hor/RecyclerViewHolder.java
@@ -1,6 +1,7 @@
 package com.asi.hor;
 
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,6 +12,8 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder  {
     // View holder for gridview recycler view as we used in listview
     public TextView title;
     public ImageView imageview;
+    //add anim to card view
+    public CardView cardView;
 
 
 
@@ -23,10 +26,14 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder  {
                 .findViewById(R.id.title);
         this.imageview = (ImageView) view
                 .findViewById(R.id.image);
+        this.cardView= (CardView) view.findViewById(R.id.card_view);
 
 
     }
 
-
+    public void clearAnimation()
+    {
+        cardView.clearAnimation();
+    }
 
 }
--- app/src/main/java/com/asi/hor/RvAdapter.java
+++ app/src/main/java/com/asi/hor/RvAdapter.java
@@ -5,7 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import java.util.ArrayList;
 
@@ -14,6 +17,7 @@ public class RvAdapter extends
     // recyclerview adapter
     private ArrayList<ItemModel> arrayList;
     private Context context;
+    private int lastPosition=-1;
 
     public RvAdapter(Context context,
                      ArrayList<ItemModel> arrayList) {
@@ -44,6 +48,8 @@ public class RvAdapter extends
         mainHolder.title.setText(model.getTitle());
 
         mainHolder.imageview.setImageBitmap(image);
+//add anim to card view
+        setAnimation(holder.cardView, position);
 
 
 
@@ -60,6 +66,30 @@ public class RvAdapter extends
         RecyclerViewHolder listHolder = new RecyclerViewHolder(mainGroup);
         return listHolder;
 
+    }
+
+
+    //
+    private void setAnimation(View viewToAnimate, int position)
+    {
+        // If the bound view wasn't previously displayed on screen, it's animated
+
+        if (position > lastPosition)
+        {
+
+            Animation animation = AnimationUtils.loadAnimation(context, (position > lastPosition) ? R.anim.slide_down : R.anim.slide_up);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
+
+
+
+    }
+    //to solve the problem of fast scroll
+    @Override
+    public void onViewDetachedFromWindow(RecyclerViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+        ((RecyclerViewHolder)holder).clearAnimation();
     }
 
 }
